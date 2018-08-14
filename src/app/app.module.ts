68999a27e21027c12d35a2d0c9bfd9a910b5898d
abhijithm <abhijithmahipal@gmail.com>
2018-08-14 16:57:48 +0530

    day2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SkillComponent } from './skill/skill.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { WritingComponent } from './writing/writing.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
+const routes: Routes = [
+  {path: 'home', component: HomeComponent },
+  {path: 'skill', component: SkillComponent },
+  {path: 'portfolio', component: PortfolioComponent },
+  {path: 'writing', component: WritingComponent },
+  {path: 'aboutme', component: AboutMeComponent },
+  {path: '',redirectTo: '/home',pathMatch:'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SkillComponent,
+    PortfolioComponent,
+    WritingComponent,
+    AboutMeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
